aa7edee3d60b4f3e01d07a5a8ec80495cc43b91b
Francisco Ponce Gomez <pakopongo@gmail.com>
2017-11-16 15:00:10 -0800

    Added QnABotAccents Sample

---
+++ CSharp/QnABotAccents/QnABotAccents/Dialogs/SimpleQnADialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.Bot.Builder.CognitiveServices.QnAMaker;
+
+namespace QnABotAccents.Dialogs
+{
+    [Serializable]
+    [QnAMakerSettings]
+    public class SimpleQnADialog : QnAMakerDialog
+    {
+        [Serializable]
+        public class QnAMakerSettingsAttribute : QnAMakerAttribute
+        {
+            public QnAMakerSettingsAttribute()
+                : base(ConfigurationManager.AppSettings["QnASubscriptionKey"], ConfigurationManager.AppSettings["QnAKbId"])
+            {
+            }
+        }
+    }
+}
--- Node/FranciscoTimerBot/app.js
+++ Node/FranciscoTimerBot/app.js
@@ -4,7 +4,6 @@ A simple echo bot for the Microsoft Bot Framework.
 
 var restify = require('restify');
 var builder = require('botbuilder');
-var azure = require('azure-storage');
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -16,8 +15,6 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword,
-    stateEndpoint: process.env.BotStateEndpoint,
-    openIdMetadata: process.env.BotOpenIdMetadata 
 });
 
 // Listen for messages from users 
@@ -34,38 +31,27 @@ var bot = new builder.UniversalBot(connector);
 
 
 // Intercept trigger event (ActivityTypes.Trigger)
-bot.on('trigger', function (message) {
+//bot.on('trigger', function (message) {
     // handle message from trigger function
-    var queuedMessage = message.value;
-    var reply = new builder.Message()
-        .address(queuedMessage.address)
-        .text('This is coming from the trigger: ' + queuedMessage.text);
-    bot.send(reply);
-});
+//    var queuedMessage = message.value;
+//    var reply = new builder.Message()
+//        .address(queuedMessage.address)
+//        .text('This is coming from the trigger: ' + queuedMessage.text);
+//    bot.send(reply);
+//});
+
+function userHasAnswered(session) {
+    if (session.dialog)
+}
 
 // Handle message from user
 bot.dialog('/', function (session) {
-    var queuedMessage = { address: session.message.address, text: session.message.text };
-    // add message to queue
-    session.sendTyping();
-    var queueSvc = azure.createQueueService(process.env.AzureWebJobsStorage);
-    queueSvc.createQueueIfNotExists('bot-queue', function(err, result, response){
-        if(!err){
-            // Add the message to the queue
-            var queueMessageBuffer = new Buffer(JSON.stringify(queuedMessage)).toString('base64');
-            queueSvc.createMessage('bot-queue', queueMessageBuffer, function(err, result, response){
-                if(!err){
-                    // Message inserted
-                    session.send('Your message (\'' + session.message.text + '\') has been added to a queue, and it will be sent back to you via a Function');
-                } else {
-                    // this should be a log for the dev, not a message to the user
-                    session.send('There was an error inserting your message into queue');
-                }
-            });
-        } else {
-            // this should be a log for the dev, not a message to the user
-            session.send('There was an error creating your queue');
-        }
-    });
+   builder.Prompts.text(session, `Who will be attending this meeting?`);
+   
+   setTimeout(() => {
+   userHasAnswered(session);
+   session.endConversation()
+   }, 5000);
+
 
 });
